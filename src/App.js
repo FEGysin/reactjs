45190eac74be3f25659755d4d2fdbb26ad4f8009
FEGysin <FEGysin@gmail.com>
2022-12-10 16:40:36 -0300

    Inicio PreEntrega2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-//import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header/Header";
-import NavBar from "./components/NavBar/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Layout/Header/Header";
+import NavBar from "./components/Layout/components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Home from "./pages/Home";
+import ProductList from "./pages/Products";
+import ProductDetail from "./pages/ProdDetail";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Header />
       <NavBar />
       <ItemListContainer>
@@ -27,6 +16,13 @@ function App() {
           Sean Bienvenidos al e-Comerce de <strong>"La Monoco"</strong>
         </span>
       </ItemListContainer>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/Products" element={<ProductList />}>
+          <Route path=":Product" element={<ProductDetail />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
